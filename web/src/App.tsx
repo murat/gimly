6a715a2b31acc486e60f4a4fba18c7a128f4c957
Osman Tangoren <otangoren@gmail.com>
2023-04-17 09:40:28 +0300

    feat: init tailwindcss

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./index.css";
 
 interface Url {
   url: {
@@ -9,7 +9,7 @@ interface Url {
   };
   short_id: string;
   click_count: number;
-};
+}
 
 interface ApiResponse {
   data: Url[];
@@ -21,27 +21,32 @@ interface ApiError {
 }
 
 const IndexPage: React.FC = () => {
-  const [title, setTitle] = useState('');
-  const [url, setUrl] = useState('');
+  const [title, setTitle] = useState("");
+  const [url, setUrl] = useState("");
   const [urls, setUrls] = useState<Url[]>([]);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
     try {
-      const response = await axios.post<Url>('/api/url', { data: { title, url } });
+      const response = await axios.post<Url>("/api/url", {
+        data: { title, url },
+      });
       setUrls([...urls, response.data]);
-      setTitle('');
-      setUrl('');
-      setError('');
+      setTitle("");
+      setUrl("");
+      setError("");
     } catch (error: any) {
       const apiError = error.response.data as ApiError;
       setError(apiError.error);
     }
   };
 
-  const handleCopyClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, url: string) => {
+  const handleCopyClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
+    url: string
+  ) => {
     event.preventDefault();
     navigator.clipboard.writeText(url);
   };
@@ -49,7 +54,7 @@ const IndexPage: React.FC = () => {
   useEffect(() => {
     const fetchUrls = async () => {
       try {
-        const response = await axios.get<ApiResponse>('/api/url');
+        const response = await axios.get<ApiResponse>("/api/url");
         setUrls(response.data.data);
       } catch (error) {
         console.error(error);
@@ -59,7 +64,7 @@ const IndexPage: React.FC = () => {
   }, []);
 
   return (
-    <div className="index-page">
+    <>
       <h1>URL Shortener</h1>
       <form onSubmit={handleSubmit}>
         <div className="form-row">
@@ -85,7 +90,9 @@ const IndexPage: React.FC = () => {
           />
           {error && <p className="error-message">{error}</p>}
         </div>
-        <button type="submit" className="submit-button">Shorten</button>
+        <button type="submit" className="submit-button">
+          Shorten
+        </button>
       </form>
       {urls.length > 0 && (
         <ul className="url-list">
@@ -93,21 +100,33 @@ const IndexPage: React.FC = () => {
             <li key={url.url.url}>
               <div className="url-container">
                 <div className="url-title">{url.url.title}</div>
-                <div className="url-url"><a href={`/u/${url.url.url}`} target="_blank">{url.url.url}</a></div>
-                {(url.click_count || 0) > 0 &&
-                  <div className="url-click-count">{url.click_count} clicks</div>
-                }
+                <div className="url-url">
+                  <a
+                    href={`/u/${url.url.url}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {url.url.url}
+                  </a>
+                </div>
+                {(url.click_count || 0) > 0 && (
+                  <div className="url-click-count">
+                    {url.click_count} clicks
+                  </div>
+                )}
               </div>
-              <button onClick={(event) => handleCopyClick(event, url.url.url)} className="copy-button">
+              <button
+                onClick={(event) => handleCopyClick(event, url.url.url)}
+                className="copy-button"
+              >
                 Copy
               </button>
             </li>
           ))}
         </ul>
-      )
-      }
-    </div >
+      )}
+    </>
   );
-}
+};
 
 export default IndexPage;
